eb361af1bdcc1dd855149f7e06c214cd4861636f
unknown <User@MTC-ICT3-PC14.intan.dir>
2020-02-19 16:27:49 +0800

    login form function

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,9 @@ export class LoginPage implements OnInit {
   userForm: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private router: Router,
+    public toastController: ToastController
   ) {
     this.initForm();
   }
@@ -31,7 +35,28 @@ export class LoginPage implements OnInit {
     var usernameIn = this.userForm.controls['username'].value;
     var passwordIn = this.userForm.controls['password'].value;
 
-    console.log(usernameIn + " " + passwordIn);
+    if((usernameIn == '') || (passwordIn == '')) {
+      this.presentToast("Please fill username and password!");
+    } else {
+      var dummyUser = "ashahir";
+      var dummyPass = "123456";
+
+      console.log(usernameIn + " " + passwordIn);
+
+      if((usernameIn == dummyUser) && (passwordIn == dummyPass)) {
+        this.router.navigateByUrl("/home");
+      } else {
+        this.presentToast("Username or password not match!");
+      }
+    }
   }
 
+  async presentToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+      color: 'warning'
+    });
+    toast.present();
+  }
 }
